8e564ee397cbecd3bca13e30f93eab693db89e74
themutherfvcker <162934982+themutherfvcker@users.noreply.github.com>
2025-10-22 15:36:32 +1100

    Add SiteHeader component for site navigation

---
+++ components/siteheader.tsx
@@ -0,0 +1,15 @@
+// components/SiteHeader.tsx
+export default function SiteHeader() {
+  return (
+    <header className="header">
+      <div className="header-inner">
+        <div className="brand">Roof Restoration – Northern Beaches</div>
+        <nav className="nav">
+          <a href="/">Home</a>
+          <a href="tel:+611300000000">Call 1300 000 000</a>
+          <a href="/sitemap.xml">Sitemap</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
